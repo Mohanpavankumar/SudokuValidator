6e01ad964782ea1160b1480de341d3ced0e89990
V.Mohanapavankumar <mohanpavankumarvankayala@gmail.com>
2025-03-08 14:03:39 -0600

    Update test_sudoku_validation.py

--- tests/test_sudoku_validation.py
+++ tests/test_sudoku_validation.py
@@ -1,5 +1,5 @@
 import sys;
-import os
+import os;
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import unittest;
 from src.sudoku_validation import is_valid_sudoku;
